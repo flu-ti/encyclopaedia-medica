5e7beca351e75c3e77206a9c0ef99a6f827e43f1
tflueck <90704810+tflueck@users.noreply.github.com>
2024-10-20 16:18:27 +0200

    added code

--- Themenblöcke/js/themenbloecke.js
+++ Themenblöcke/js/themenbloecke.js
@@ -47,3 +47,18 @@ function closeModal() {
   var modal = document.getElementById('myModal');
   modal.style.display = "none";
 }
+
+
+
+	// Detect if running on GitHub Pages or locally
+window.onload = function() {
+  var baseTag = document.createElement('base');
+  if (window.location.hostname === "flu-ti.github.io") {
+    // Set base URL for GitHub Pages
+    baseTag.href = "/encyclopaedia-medica/";
+  } else {
+    // Set base URL for local development (VS Code live preview)
+    baseTag.href = "/";
+  }
+  document.head.appendChild(baseTag);
+};
