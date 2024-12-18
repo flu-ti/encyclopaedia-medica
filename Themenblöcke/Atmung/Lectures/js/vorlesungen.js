8beb67dc1631c9b345087d1404174f21e6b51546
tflueck <90704810+tflueck@users.noreply.github.com>
2024-12-18 08:58:20 +0100

    New Content

---
+++ Themenblöcke/Atmung/Lectures/js/vorlesungen.js
@@ -0,0 +1,34 @@
+
+const nav = document.querySelector('.sidenav')
+fetch('../../../lecturelist_atmung.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML = data
+})
+
+const head = document.querySelector('.header')
+fetch('../header.html')
+.then(res=>res.text())
+.then(data=>{
+    head.innerHTML = data
+})
+
+
+
+function toggleNav() {
+var sidenav = document.getElementById("mySidenav");
+// Toggle the display property between 'block' and 'none'
+if (sidenav.style.display === "block") {
+sidenav.style.display = "none";  // Close the sidenav
+} else {
+sidenav.style.display = "block";  // Open the sidenav
+}
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+  }
